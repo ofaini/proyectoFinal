039c8d5e85369c7ae7536e3c6577c6cfc98f13fe
Vickykathe <kathevela30@hotmail.com>
2019-11-28 12:31:25 -0500

    activity mensajes

--- app/src/main/java/com/example/afinal/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/afinal/Adapter/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.example.afinal.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.afinal.MessageActivity;
 import com.example.afinal.Model.User;
 import com.example.afinal.R;
 
@@ -35,13 +37,23 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        User user = mUsers.get(position);
+        final User user = mUsers.get(position);
         holder.username.setText(user.getUsername());
         if(user.getImageUrl().equals("default")){
             holder.profile_image.setImageResource(R.mipmap.ic_launcher);
         }else{
             Glide.with(mContext).load(user.getImageUrl()).into(holder.profile_image);
         }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid", user.getId());
+                mContext.startActivity(intent);
+
+            }
+        });
     }
 
     @Override
